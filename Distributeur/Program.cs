3ae71b8cd1e2cc8ae6143fece5814c49f8d8b38d
Adrien Legrain <skynico200@gmail.com>
2024-09-24 11:18:24 +0200

    Change version : V2 to V3

--- Distributeur/Program.cs
+++ Distributeur/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            // DV1
             //     int stock1 = 5; 
             //     int stock2 = 2; 
             //     int stock3 = 0; 
@@ -54,45 +55,46 @@
             //             Console.WriteLine("Choix indisponible !")
             //             break;
 
-            int stock1 = 0;
-            int stock2 = 2;
-            int stock3 = 3;
-            string? choix;
+            // DV2
+            // int stock1 = 0;
+            // int stock2 = 2;
+            // int stock3 = 3;
+            // string? choix;
 
-            do
-            {
-                Console.WriteLine("Choisissez une boisson. Boisson1: 1, Boisson2: 2, Boisson3: 3, FINIR : 4");
-                choix = Console.ReadLine();
-                if (choix == "1")
-                {
-                    if (stock1 > 0)
-                    {
-                        Console.WriteLine("Voici votre boisson1 , santé!");
-                        stock1 -= 1;
-                    }
-                    else Console.WriteLine("Plus de boisson1");
-                }
-                else if (choix == "2")
-                {
-                    if (stock2 > 0)
-                    {
-                        Console.WriteLine("Voici votre boisson2 , santé!");
-                        stock2 -= 1;
-                    }
-                    else Console.WriteLine("Plus de boisson2");
-                }
-                else if (choix == "3")
-                {
-                    if (stock3 > 0)
-                    {
-                        Console.WriteLine("Voici votre boisson3 , santé!");
-                        stock3 -= 1;
-                    }
-                    else Console.WriteLine("Plus de boisson3");
-                }
-            } while (((stock1 + stock2 + stock3) > 0) && (choix != "4"));
+            // do
+            // {
+            //     Console.WriteLine("Choisissez une boisson. Boisson1: 1, Boisson2: 2, Boisson3: 3, FINIR : 4");
+            //     choix = Console.ReadLine();
+            //     if (choix == "1")
+            //     {
+            //         if (stock1 > 0)
+            //         {
+            //             Console.WriteLine("Voici votre boisson1 , santé!");
+            //             stock1 -= 1;
+            //         }
+            //         else Console.WriteLine("Plus de boisson1");
+            //     }
+            //     else if (choix == "2")
+            //     {
+            //         if (stock2 > 0)
+            //         {
+            //             Console.WriteLine("Voici votre boisson2 , santé!");
+            //             stock2 -= 1;
+            //         }
+            //         else Console.WriteLine("Plus de boisson2");
+            //     }
+            //     else if (choix == "3")
+            //     {
+            //         if (stock3 > 0)
+            //         {
+            //             Console.WriteLine("Voici votre boisson3 , santé!");
+            //             stock3 -= 1;
+            //         }
+            //         else Console.WriteLine("Plus de boisson3");
+            //     }
+            // } while (((stock1 + stock2 + stock3) > 0) && (choix != "4"));
 
-            Console.WriteLine("Merci d'utiliser distributeur 3000!");
+            // Console.WriteLine("Merci d'utiliser distributeur 3000!");
         }
     }
 }
